321693e4f35d4c946112d84b62ea32a51a8d4536
B P <aslermelean@gmail.com>
2024-10-30 20:19:03 +0300

    реализованно через массив

--- desktop-session-exit.sh
+++ desktop-session-exit.sh
@@ -1,18 +1,32 @@
+#!/usr/bin/env bash
 
 #/usr/local/bin/desktop-session-exit
 SH="desktop-session-exit"
-echo "$SH ..."
+echo "/usr/local/bin/$SH ..."
 echo ''
-echo "1. -l | --logout     Logout of the current session"
-echo "2. -L | --lock       Lock the current session"
-echo "3. -H | --hibernate  Set the machine into hibernate"
-echo "4. -s | --shutdown   Shutdown your machine"
-echo "5. -S | --suspend    Set the machine into suspend"
-echo "6. -r | --reboot     Reboot your machine"
-echo "7. -R | --restart    Restart the session"
+
+A1='-l | l | --logout     Logout of the current session'
+A2='-L | L | --lock       Lock the current session'
+A3="-H | H | --hibernate  Set the machine into hibernate"
+A4="-s | s | --shutdown   Shutdown your machine"
+A5="-S | S | --suspend    Set the machine into suspend"
+A6="-r | r | --reboot     Reboot your machine"
+A7="-R | R | --restart    Restart the session"
+
+ARR=("$A1" "$A2" "$A3" "$A4" "$A5" "$A6" "$A7")
+
+#"2. -L | L | --lock       Lock the current session" "3. -H | H | --hibernate  Set the machine into hibernate" "4. -s | s | --shutdown   Shutdown your machine" "5. -S | S | --suspend    Set the machine into suspend" "6. -r | r | --reboot     Reboot your machine" "7. -R | R | --restart    Restart the session")
+
+for i in "${!ARR[@]}"; do
+	echo "$((i + 1)). ${ARR[i]}"
+done
+
+#read a
+
 echo ''
+
 # Читаем выбор пользователя
-read -p "Введите номер, короткую опцию или длинню (1 | -l | --logout): " CHOICE
+read -p "Введите номер, короткую опцию или длинню (Например: 1 | -l | l | --logout): " CHOICE
 echo
 case $CHOICE in
 	1 | -l | --logout)
