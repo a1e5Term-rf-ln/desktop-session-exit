ae6e0da7b39e5711878b80a9453883ef1a4e1d4d
B P <aslermelean@gmail.com>
2024-10-30 19:56:07 +0300

    рб

--- desktop-session-exit.sh
+++ desktop-session-exit.sh
@@ -1,90 +1,45 @@
 
-/usr/local/bin/desktop-session-exit
+#/usr/local/bin/desktop-session-exit
 SH="desktop-session-exit"
-
 echo "$SH ..."
+echo ''
 echo "1. -l | --logout     Logout of the current session"
 echo "2. -L | --lock       Lock the current session"
-echo "3. -h | --hibernate  Set the machine into hibernate"
+echo "3. -H | --hibernate  Set the machine into hibernate"
 echo "4. -s | --shutdown   Shutdown your machine"
 echo "5. -S | --suspend    Set the machine into suspend"
 echo "6. -r | --reboot     Reboot your machine"
 echo "7. -R | --restart    Restart the session"
-
+echo ''
 # Читаем выбор пользователя
 read -p "Введите номер, короткую опцию или длинню (1 | -l | --logout): " CHOICE
-
+echo
 case $CHOICE in
-	-l|--logout)
-		$desktop_session_logout
+	1 | -l | --logout)
+		$SH -l
 		;;
-	-L|--lock)
-		xlock
+	2 | -L | --lock)
+		$SH -L
 		;;
-	h|--help)
+	3 | -H | --hibernate)
+	    $SH -H
+	    ;;
+	4 | -s | --shutdown)
+		$SH -s
+		;;
+	5 | -S | --suspend)
+		$SH -S
+		;;
+	6 | -r | --reboot)
+		$SH -6
+		;;
+	7 | -R | --Restart)
+		$SH -R
+		;;
+	8 | h | --help)
 		help
 		;;
-	#-H|--hibernate)
-	#    sudo pm-hibernate
-	#    ;;
-	-s|--shutdown)
-		if [ -e /etc/live/config/save-persist -o -e /live/config/persist-save.conf ] && which persist-config &> /dev/null; then
-			sudo persist-config --shutdown --command poweroff
-		else
-			sync
-			#sudo chvt 1#this causes exit to reappear on halt
-			sudo poweroff
-		fi
-		;;
-	-S|--suspend)
-		sudo pm-suspend
-		;;
-	-r|--reboot)
-		if [ -e /etc/live/config/save-persist -o -e /live/config/persist-save.conf ] && which persist-config &> /dev/null; then
-			sudo persist-config --shutdown --command reboot
-		else
-			sync
-			#sudo chvt 1#this causes exit to reappear on reboot
-			sudo reboot
-		fi
-		;;
-	-R|--Restart)
-		$desktop_session_restart
-		;;
 	*)
-		if [ "$DISPLAY" ]; then
-			$desktop_session_exit_gui &
-			exit
-		else
-			echo "$1 Not an option"
-			help;
-		fi
+		exit
 		;;
 esac
-
-
-case
-	1
-	Lock Screen 
-	desktop-session-exit -L
-	
-	2
-	Restart Session 
-	desktop-session-exit -R
-
-	3
-	Reboot 
-	desktop-session-exit -r
-
-	4
-	Log Out 
-	desktop-session-exit -l
-
-	5
-	Suspend 
-	desktop-session-exit -S
-
-	6
-	Shutdown 
-	desktop-session-exit -s
-	
