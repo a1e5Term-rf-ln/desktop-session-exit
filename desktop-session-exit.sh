0ab8367f4fcf46e87e4314924b8d7e91ee809c62
B P <aslermelean@gmail.com>
2024-10-30 19:36:11 +0300

    найеден баг geany

--- desktop-session-exit.sh
+++ desktop-session-exit.sh
@@ -1,8 +1,67 @@
 
+/usr/local/bin/desktop-session-exit
+SH="desktop-session-exit"
 
+echo "$SH ..."
+echo "1. -l | --logout     Logout of the current session"
+echo "2. -L | --lock       Lock the current session"
+echo "3. -h | --hibernate  Set the machine into hibernate"
+echo "4. -s | --shutdown   Shutdown your machine"
+echo "5. -S | --suspend    Set the machine into suspend"
+echo "6. -r | --reboot     Reboot your machine"
+echo "7. -R | --restart    Restart the session"
 
-echo введите номер
-read
+# Читаем выбор пользователя
+read -p "Введите номер, короткую опцию или длинню (1 | -l | --logout): " CHOICE
+
+case $CHOICE in
+	-l|--logout)
+		$desktop_session_logout
+		;;
+	-L|--lock)
+		xlock
+		;;
+	h|--help)
+		help
+		;;
+	#-H|--hibernate)
+	#    sudo pm-hibernate
+	#    ;;
+	-s|--shutdown)
+		if [ -e /etc/live/config/save-persist -o -e /live/config/persist-save.conf ] && which persist-config &> /dev/null; then
+			sudo persist-config --shutdown --command poweroff
+		else
+			sync
+			#sudo chvt 1#this causes exit to reappear on halt
+			sudo poweroff
+		fi
+		;;
+	-S|--suspend)
+		sudo pm-suspend
+		;;
+	-r|--reboot)
+		if [ -e /etc/live/config/save-persist -o -e /live/config/persist-save.conf ] && which persist-config &> /dev/null; then
+			sudo persist-config --shutdown --command reboot
+		else
+			sync
+			#sudo chvt 1#this causes exit to reappear on reboot
+			sudo reboot
+		fi
+		;;
+	-R|--Restart)
+		$desktop_session_restart
+		;;
+	*)
+		if [ "$DISPLAY" ]; then
+			$desktop_session_exit_gui &
+			exit
+		else
+			echo "$1 Not an option"
+			help;
+		fi
+		;;
+esac
+
 
 case
 	1
